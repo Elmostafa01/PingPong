1153a929c9d854f1aac5a3201f86d77f9e06a989
El Mostafa Ramdi <93084739+Elmostafa01@users.noreply.github.com>
2023-06-05 03:41:26 +0100

    styling rank and rankgame components

--- src/components/Rank.tsx
+++ src/components/Rank.tsx
@@ -1,11 +1,46 @@
 import React from 'react'
 import rectangle from '../images/rectangle.svg';
+import arrowtop from '../images/arrowTop.svg';
+import arrowtoptwo from '../images/arrowTopTwo.svg';
+import blackrectangle from '../images/blackrectangle.svg';
+
 
 
 const Rank = () => {
   return (
     <div className='Rank'>
-      
+      <div className="rankWrapper">
+        <div className="rank-one">
+            <p>Rank<span className='rankNumber'>#1</span></p>
+            <div className="player">
+              <span className='playerName'>AYA11</span>
+              <img src={arrowtop} height={25}/>
+            </div>
+            <div className="rectangle">
+              <img src={rectangle} alt='svg'  />
+            </div>
+        </div>
+        <div className="rank-two">
+        <p>Rank<span className='rankNumber'>#1</span></p>
+            <div className="player">
+              <span className='playerName'>ISSAM</span>
+              <img src={arrowtop} height={25}/>
+            </div>
+            <div className="rectangle">
+              <img src={arrowtoptwo} alt='svg'  />
+            </div>
+        </div>
+        <div className="rank-three">
+        <p>Rank<span className='rankNumber'>#1</span></p>
+            <div className="player">
+              <span className='playerName'>OUSSAMA06</span>
+              <img src={arrowtop} height={25}/>
+            </div>
+            <div className="rectangle">
+              <img src={blackrectangle} alt='svg'  />
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/RankedGame.tsx
+++ src/components/RankedGame.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import players from '../images/players.jpg'
+
 
 const RankedGame = () => {
   return (
     <div className='RankedGame'>
-      
+      <div className="rankedGameWrapper">
+        <div className="title">
+          <h3>Game</h3>
+        </div>
+        <div className="btn">
+          <button>PLAY</button>
+        </div>
+        <div className="image">
+          <img src={players} alt="" />
+        </div>
+      </div>
     </div>
   )
 }
