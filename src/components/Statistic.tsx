ecf36c40de125de94ef3990d33a57d84e594efa0
El Mostafa Ramdi <93084739+Elmostafa01@users.noreply.github.com>
2023-06-07 23:49:52 +0100

    styling rank component

--- src/components/Statistic.tsx
+++ src/components/Statistic.tsx
@@ -3,7 +3,20 @@ import React from 'react'
 const Statistic = () => {
   return (
     <div className='Statistic'>
-      
+      <div className="statistic-wrapper">
+        <div className="winrate">
+          <div className="skill">
+            <div className="outer">
+              <div className="inner">
+                
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="loggedin">
+
+        </div>
+      </div>
     </div>
   )
 }
