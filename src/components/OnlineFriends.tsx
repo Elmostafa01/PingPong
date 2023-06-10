781de04a651f2649f8bfdbc9ee2f80c9042d847e
El Mostafa Ramdi <93084739+Elmostafa01@users.noreply.github.com>
2023-06-10 01:55:45 +0100

    finished Friends component

--- src/components/OnlineFriends.tsx
+++ src/components/OnlineFriends.tsx
@@ -8,7 +8,9 @@ import sendchat from '../images/sendchat.svg'
 const OnlineFriends = () => {
   return (
     <div className='OnlineFriends'>
-      <div className="title"><h3>✨Friends✨</h3></div>
+      <div className="title">
+        <h3>✨Friends✨</h3>
+      </div>
       <div className="cards-wrapper">
         <div className="card-friend">
           <div className="friend">
@@ -22,9 +24,231 @@ const OnlineFriends = () => {
             </div>
           </div>
           <div className="send-msg">
-            <input type="text" placeholder='Send a Msg...'/>
+            <input type="text" placeholder='Send a Msg . . .'/>
             <div className="sendchat">
+              <button>
               <img src={sendchat} alt="chat" />
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="card-friend">
+          <div className="friend">
+            <div className="avatar">
+              <span className='online'></span>
+              <img src={girone} alt='friend image' height={60} />
+            </div>
+            <div className="card-name">
+              <span className='name'>Nobles</span>
+              <span className='ingame'>Free</span>
+            </div>
+          </div>
+          <div className="send-msg">
+            <input type="text" placeholder='Send a Msg . . .'/>
+            <div className="sendchat">
+              <button>
+              <img src={sendchat} alt="chat" />
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="card-friend">
+          <div className="friend">
+            <div className="avatar">
+              <span className='online'></span>
+              <img src={menone} alt='friend image' height={60} />
+            </div>
+            <div className="card-name">
+              <span className='name'>Bilal</span>
+              <span className='ingame'>In Game</span>
+            </div>
+          </div>
+          <div className="send-msg">
+            <input type="text" placeholder='Send a Msg . . .'/>
+            <div className="sendchat">
+              <button>
+              <img src={sendchat} alt="chat" />
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="card-friend">
+          <div className="friend">
+            <div className="avatar">
+              <span className='online'></span>
+              <img src={girltwo} alt='friend image' height={60} />
+            </div>
+            <div className="card-name">
+              <span className='name'>Pheonix</span>
+              <span className='ingame'>In Game</span>
+            </div>
+          </div>
+          <div className="send-msg">
+            <input type="text" placeholder='Send a Msg . . .'/>
+            <div className="sendchat">
+              <button>
+              <img src={sendchat} alt="chat" />
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="card-friend">
+          <div className="friend">
+            <div className="avatar">
+              <span className='online'></span>
+              <img src={menone} alt='friend image' height={60} />
+            </div>
+            <div className="card-name">
+              <span className='name'>Phen</span>
+              <span className='ingame'>Free</span>
+            </div>
+          </div>
+          <div className="send-msg">
+            <input type="text" placeholder='Send a Msg . . .'/>
+            <div className="sendchat">
+              <button>
+              <img src={sendchat} alt="chat" />
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="card-friend">
+          <div className="friend">
+            <div className="avatar">
+              <span className='online'></span>
+              <img src={girltwo} alt='friend image' height={60} />
+            </div>
+            <div className="card-name">
+              <span className='name'>samaha</span>
+              <span className='ingame'>Free</span>
+            </div>
+          </div>
+          <div className="send-msg">
+            <input type="text" placeholder='Send a Msg . . .'/>
+            <div className="sendchat">
+              <button>
+              <img src={sendchat} alt="chat" />
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="card-friend">
+          <div className="friend">
+            <div className="avatar">
+              <span className='online'></span>
+              <img src={mentwo} alt='friend image' height={60} />
+            </div>
+            <div className="card-name">
+              <span className='name'>Zetrix</span>
+              <span className='ingame'>In Game</span>
+            </div>
+          </div>
+          <div className="send-msg">
+            <input type="text" placeholder='Send a Msg . . .'/>
+            <div className="sendchat">
+              <button>
+              <img src={sendchat} alt="chat" />
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="card-friend">
+          <div className="friend">
+            <div className="avatar">
+              <span className='online'></span>
+              <img src={girone} alt='friend image' height={60} />
+            </div>
+            <div className="card-name">
+              <span className='name'>Nobles</span>
+              <span className='ingame'>Free</span>
+            </div>
+          </div>
+          <div className="send-msg">
+            <input type="text" placeholder='Send a Msg . . .'/>
+            <div className="sendchat">
+              <button>
+              <img src={sendchat} alt="chat" />
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="card-friend">
+          <div className="friend">
+            <div className="avatar">
+              <span className='online'></span>
+              <img src={menone} alt='friend image' height={60} />
+            </div>
+            <div className="card-name">
+              <span className='name'>Bilal</span>
+              <span className='ingame'>In Game</span>
+            </div>
+          </div>
+          <div className="send-msg">
+            <input type="text" placeholder='Send a Msg . . .'/>
+            <div className="sendchat">
+              <button>
+              <img src={sendchat} alt="chat" />
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="card-friend">
+          <div className="friend">
+            <div className="avatar">
+              <span className='online'></span>
+              <img src={girltwo} alt='friend image' height={60} />
+            </div>
+            <div className="card-name">
+              <span className='name'>Pheonix</span>
+              <span className='ingame'>In Game</span>
+            </div>
+          </div>
+          <div className="send-msg">
+            <input type="text" placeholder='Send a Msg . . .'/>
+            <div className="sendchat">
+              <button>
+              <img src={sendchat} alt="chat" />
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="card-friend">
+          <div className="friend">
+            <div className="avatar">
+              <span className='online'></span>
+              <img src={menone} alt='friend image' height={60} />
+            </div>
+            <div className="card-name">
+              <span className='name'>Phen</span>
+              <span className='ingame'>Free</span>
+            </div>
+          </div>
+          <div className="send-msg">
+            <input type="text" placeholder='Send a Msg . . .'/>
+            <div className="sendchat">
+              <button>
+              <img src={sendchat} alt="chat" />
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="card-friend">
+          <div className="friend">
+            <div className="avatar">
+              <span className='online'></span>
+              <img src={girltwo} alt='friend image' height={60} />
+            </div>
+            <div className="card-name">
+              <span className='name'>samaha</span>
+              <span className='ingame'>Free</span>
+            </div>
+          </div>
+          <div className="send-msg">
+            <input type="text" placeholder='Send a Msg . . .'/>
+            <div className="sendchat">
+              <button>
+              <img src={sendchat} alt="chat" />
+              </button>
             </div>
           </div>
         </div>
