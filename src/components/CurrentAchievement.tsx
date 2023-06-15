5ccda36de8122fc20a22081955e4cbf568616105
El Mostafa Ramdi <93084739+Elmostafa01@users.noreply.github.com>
2023-06-15 17:47:29 +0100

    profil card finished

--- src/components/CurrentAchievement.tsx
+++ src/components/CurrentAchievement.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import challenger from '../images/achievements/9-challenger.svg'
 
 const CurrentAchievement = () => {
@@ -17,10 +16,9 @@ const CurrentAchievement = () => {
           </div>
           <div className="text">
             <p>
-              Master of the Ping Pong Realm, 
-              your unparalleled skills and dedication 
-              have earned you the prestigious title in the game.
-              Congratulations on your extraordinary achievement!
+            Congratulations, Ping Pong Realm Faker!
+Unmatched skills, prestigious title.
+Extraordinary achievement!
             </p>
           </div>
         </div>
