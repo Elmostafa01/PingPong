759d2c3aee5776235745661f7105bc9983aec81e
El Mostafa Ramdi <93084739+Elmostafa01@users.noreply.github.com>
2023-07-04 06:52:27 +0100

    LandingPage adding Nav

---
+++ src/components/LandingNav.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import logo from '../images/logo.svg'
+import { Link } from 'react-router-dom';
+import { BiChevronDown } from 'react-icons/bi'
+import { FaGithub } from "react-icons/fa6";
+import { FaPenToSquare } from "react-icons/fa6";
+
+
+
+const LandingNav: React.FC = () => {
+  return (
+      <>
+      <nav className="nav">
+            <div className="logo">
+               <img className="img" src={logo} />
+            </div>
+            <div className="about">
+                <button className="dropbtn">
+                    About
+                    <BiChevronDown className="fas" />
+                </button>
+                <div className="dropdown-content">
+                  <a className='anchor' href="https://github.com/Elmostafa01/PingPong" target="_blank" ><FaGithub />Github</a>
+                  <Link className='anchor' to="#"><FaPenToSquare />Sign In</Link>
+                </div>
+            </div>
+        </nav>
+      </>
+  )
+}
+
+export default LandingNav
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import LandingNav from '../components/LandingNav'
 
 const LandingPage: React.FC = () => {
   return (
     <div className='LandingPage'>
-      
+      <LandingNav />
+      <div className="mid-align-content">
+        
+      </div>
     </div>
   )
 }
