f42bdb2fcd989e1182c02ed2d6dbb6b14938f0fb
El Mostafa Ramdi <93084739+Elmostafa01@users.noreply.github.com>
2023-07-06 23:29:07 +0100

    styling authenticationPage Layout is done

--- src/pages/Authentication.tsx
+++ src/pages/Authentication.tsx
@@ -3,29 +3,40 @@ import Intra from '../components/Intra'
 import Google from '../components/Google'
 import arrowLeft from '../images/arrow-left.svg'
 import hero from '../images/SVG-HERO.svg'
+import { useNavigate } from 'react-router-dom';
 
 const Authentication: React.FC = () => {
+  const history = useNavigate();
+
+  const goBack = () => {
+    history('/');
+  }
+
   return (
     <div className='Authentincation-page'>
-      <div className="authentication">
-        <h2>welcome 👋</h2>
-        <p>Please log in with</p>
-        <Intra />
-        <div className="line-seperated">
-          <span></span>
-          <p>Or</p>
-          <span></span>
+      <div className="auth-component">
+        <div className="auth-providers">
+          <div className="text">
+            <h2>welcome 👋</h2>
+            <p>Please log in with</p>
+          </div>
+          <Intra />
+          <div className="line-seperated">
+            <span></span>
+            <p>Or</p>
+            <span></span>
+          </div>
+          <Google />
+          <div className="goback">
+            <button onClick={goBack} className='back'>
+              <img src={arrowLeft} />
+            </button>
+            <p>Go Back</p>
+          </div>
         </div>
-        <Google />
-        <div className="goback">
-          <button className='back'>
-            <img src={arrowLeft} />
-          </button>
-          <p>Go Back</p>
+        <div className="hero-img">
+          <img src={hero} height={400} />
         </div>
-      </div>
-      <div className="hero-img">
-        {/*<img src={hero} height={400} />*/}
       </div>
     </div>
   )
