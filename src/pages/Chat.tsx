15d7e256c925fc3b2969d4685f69e4894e83f04a
El Mostafa Ramdi <93084739+Elmostafa01@users.noreply.github.com>
2023-07-22 05:38:46 +0100

    contact-list chat

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,7 +1,144 @@
+import menone from '../images/menone.svg';
+import mentwo from '../images/mentwo.svg';
+import girone from '../images/girlone.svg';
+import girltwo from '../images/girltwo.svg';
+import { FiSearch } from "react-icons/fi";
+
+
+interface Contact {
+  name: string;
+  lastMsg: string;
+  image: string;
+  message: string;
+  msgNumber: number;
+}
+
+const allFriendsData: Contact[] = [
+  {
+    name: 'Zetrix',
+    lastMsg: 'Hi, Och ba9i katghani ?',
+    image: mentwo,
+    message: '',
+    msgNumber: 2
+  },
+  {
+    name: 'Nobles',
+    lastMsg: 'Looking for some help Anyone ',
+    image: girltwo,
+    message: '',
+    msgNumber: 2
+  },
+  {
+    name: 'Bilal',
+    lastMsg: 'Are You still aLive ?',
+    image: menone,
+    message: '',
+    msgNumber: 0,
+  },
+  {
+    name: 'Phoenix',
+    lastMsg: 'Selling New Car , check this beauty',
+    image: girone,
+    message: '',
+    msgNumber: 5
+  },
+  {
+    name: 'Phen',
+    lastMsg: 'How are you today ?',
+    image: menone,
+    message: '',
+    msgNumber: 0,
+  },
+  {
+    name: 'Salah',
+    lastMsg: 'Hi, Are There ?',
+    image: mentwo,
+    message: '',
+    msgNumber: 0,
+  },
+  {
+    name: 'Khalid',
+    lastMsg: 'Going to Help you with new Products',
+    image: menone,
+    message: '',
+    msgNumber: 0,
+  },
+  {
+    name: 'Craziman',
+    lastMsg: 'Waaaaa khuna !!',
+    image: mentwo,
+    message: '',
+    msgNumber: 4,
+  },
+  {
+    name: 'Tofaha',
+    lastMsg: 'Bonne nuit , siri dwi m3aha :)',
+    image: girone,
+    message: '',
+    msgNumber: 3,
+  },
+  {
+    name: 'Ohio',
+    lastMsg: 'ghabarti a sahbi !',
+    image: mentwo,
+    message: '',
+    msgNumber: 0,
+  },
+  {
+    name: 'Strawberry',
+    lastMsg: 'wtf !??',
+    image: girltwo,
+    message: '',
+    msgNumber: 0,
+  },
+  {
+    name: 'Numnum',
+    lastMsg: 'badalti numero ?',
+    image: menone,
+    message: '',
+    msgNumber: 1
+  },
+];
+
 
 const Chat = () => {
   return (
     <div className='chat'>
+      <div className="wrapper">
+        <div className="chat-contact">
+          <div className="header">
+            <div className="chats">
+              <p>Chats</p>
+              <span>20</span>
+            </div>
+          </div>
+          <div className="searchbar">
+            <FiSearch className='search-tool' />
+            <input type="text" placeholder='Looking For a Player ?' />
+          </div>
+          <div className="contacts-list">
+            {allFriendsData.map((contact, i) => (
+              <div className="contact" key={i}>
+                <div className="avatar">
+                  <img src={contact.image} alt="" />
+                </div>
+                <div className="right-labels">
+                  <div className="name-label">
+                    <div className="name">{contact.name}</div>
+                    <p>11:15 AM</p>
+                  </div>
+                  <div className="msg-label">
+                    <div className="last-msg">{contact.lastMsg}</div>
+                    <span style={{background: contact.msgNumber == 0 ? 'transparent' : ''}}>
+                      {contact.msgNumber == 0 ? '' : contact.msgNumber}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
